c67e288395737cb3af598652c56e743094432c1b
Deepti <93430410+deepti-shitole@users.noreply.github.com>
2025-03-12 22:30:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "aws_subnet" "private" {
 # Private Subnet in AZ1
 resource "aws_subnet" "private_az1" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.2.0/24"
+  cidr_block        = "10.0.4.0/24"
   availability_zone = "ap-south-1a"
 }
 
